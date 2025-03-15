dc61b9f339fc8084034d067e76fbad2a4fcc880d
zplutor <zplutor@qq.com>
2025-03-15 14:11:31 +0800

    Add BasicValueTraits mapping.

---
+++ src/sqt/orm/value/trivial/basic/basic_value_traits_mapping.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <sqt/orm/value/trivial/basic/primitive_value_traits.h>
+#include <sqt/orm/value/trivial/basic/primitive_value_type.h>
+#include <sqt/orm/value/trivial/basic/nullable_value_traits.h>
+#include <sqt/orm/value/trivial/basic/nullable_value_type.h>
+
+namespace sqt {
+
+template<typename T>
+class BasicValueTraitsFor;
+
+
+template<PrimitiveValueType T>
+class BasicValueTraitsFor<T> {
+public:
+    using type = PrimitiveValueTraits<T>;
+};
+
+
+template<NullableValueType T>
+class BasicValueTraitsFor<T> {
+public:
+    using type = NullableValueTraits<T>;
+};
+
+
+template<typename T>
+using BasicValueTraitsForT = typename BasicValueTraitsFor<T>::type;
+
+}
