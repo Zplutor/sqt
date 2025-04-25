d0fa254e4e396d7aec7d01dc158ffb298889b3b5
zplutor <zplutor@qq.com>
2025-04-25 13:29:18 +0800

    Add documentation.

--- src/sqt/orm/value/trivial/basic/primitive_value_traits.h
+++ src/sqt/orm/value/trivial/basic/primitive_value_traits.h
@@ -1,25 +1,88 @@
 #pragma once
 
+/**
+@file
+    Defines the `sqt::PrimitiveValueTraits<>` class template specialization for various types.
+*/
+
 #include <concepts>
 #include <sqt/foundation/data_type.h>
 #include <sqt/foundation/statement.h>
 
 namespace sqt {
 
+/**
+The primary template for defining tratis for primitive value types.
+
+@tparam T
+    The type of the value.
+
+@details
+    This primary template is intentionally left undefined. It is used to trigger a compile-time 
+    error when instantiated with a type that is not recognized as a primitive value type.
+    
+    There are three specializations provided for primitive value types:
+    - `sqt::PrimitiveValueTraits<INTEGER>`, for integral types.
+    - `sqt::PrimitiveValueTraits<FLOAT>`, for floating-point types.
+    - `sqt::PrimitiveValueTraits<std::string>`, for `std::string`.
+
+@see sqt::PrimitiveValueTraits<INTEGER>
+@see sqt::PrimitiveValueTraits<FLOAT>
+@see sqt::PrimitiveValueTraits<std::string>
+@see sqt::PrimitiveValueType
+*/
 template<typename T>
 class PrimitiveValueTraits;
 
 
-template<std::integral T>
-class PrimitiveValueTraits<T> {
+/**
+The specialization for defining traits for integral value types.
+
+@tparam INTEGER
+    The type that satisfies the `std::integral` concept.
+
+@details
+    This specialization satisfies the `sqt::BasicValueTraitsType` concept.
+
+@see sqt::BasicValueTraitsType
+@see sqt::PrimitiveValueTraits<FLOAT>
+@see sqt::PrimitiveValueTraits<std::string>
+*/
+template<std::integral INTEGER>
+class PrimitiveValueTraits<INTEGER> {
 public:
-    using ValueType = T;
+    using ValueType = INTEGER;
 
     static constexpr sqt::DataType DataType = sqt::DataType::Integer;
     static constexpr bool IsNullable = false;
 
-    static void BindValue(Statement& statement, int parameter_index, const T& value) {
-        if constexpr (sizeof(T) >= sizeof(std::int64_t)) {
+    /**
+    Binds an integral value to the statement at the specified parameter index.
+
+    @param statement
+        The statement to which the value is bound.
+
+    @param parameter_index
+        The index of the parameter to which the value is bound.
+
+    @param value
+        The value to bind.
+
+    @throw sqt::SQLError
+        Thrown if the binding fails.
+
+    @details
+        Depending on the size of the integral type, a different method of the statement is used 
+        to bind the value:
+        - `BindParameter(int, int)` is used for types smaller than `std::int64_t`.
+        - `BindParameter(int, std::int64_t)` is used for types equal to or larger than 
+          `std::int64_t`.
+
+    @see sqt::Statement::BindParameter(int, int)
+    @see sqt::Statement::BindParameter(int, std::int64_t)
+    */
+    static void BindValue(Statement& statement, int parameter_index, INTEGER value) {
+        if constexpr (sizeof(INTEGER) >= sizeof(std::int64_t)) {
             statement.BindParameter(parameter_index, static_cast<std::int64_t>(value));
         }
         else {
@@ -27,34 +90,123 @@ public:
         }
     }
 
-    static T RetrieveValue(const Statement& statement, int column_index) {
-        if constexpr (sizeof(T) >= sizeof(std::int64_t)) {
-            return static_cast<T>(statement.GetColumnInt64(column_index));
+    /**
+    Retrieves an integral value from the statement at the specified column index.
+
+    @param statement
+        The statement from which the value is retrieved.
+
+    @param column_index
+        The index of the column from which to retrieve the value.
+
+    @return
+        The retrieved value.
+
+    @details
+        Depending on the size of the integral type, a different method of the statement is used 
+        to retrieve the value:
+        - `GetColumnInt()` is used for types smaller than `std::int64_t`.
+        - `GetColumnInt64()` is used for types equal to or larger than `std::int64_t`.
+
+        @warning
+        The retrieved value will be truncated if the original value stored in the database exceeds 
+        the maximum representable value of the integral type.
+
+    @see sqt::Statement::GetColumnInt()
+    @see sqt::Statement::GetColumnInt64()
+    */
+    static INTEGER RetrieveValue(const Statement& statement, int column_index) noexcept {
+        if constexpr (sizeof(INTEGER) >= sizeof(std::int64_t)) {
+            return static_cast<INTEGER>(statement.GetColumnInt64(column_index));
         }
-        return static_cast<T>(statement.GetColumnInt(column_index));
+        return static_cast<INTEGER>(statement.GetColumnInt(column_index));
     }
 };
 
 
-template<std::floating_point T> requires (!std::same_as<T, long double>)
-class PrimitiveValueTraits<T> {
+/**
+The specialization for defining traits for floating-point value types.
+
+@tparam FLOAT
+    The type that satisfies the `std::floating_point` concept, except `long double`.
+
+@details
+    This specialization satisfies the `sqt::BasicValueTraitsType` concept.
+
+@see sqt::BasicValueTraitsType
+@see sqt::PrimitiveValueTraits<INTEGER>
+@see sqt::PrimitiveValueTraits<std::string>
+*/
+template<std::floating_point FLOAT> requires (!std::same_as<FLOAT, long double>)
+class PrimitiveValueTraits<FLOAT> {
 public:
-    using ValueType = T;
+    using ValueType = FLOAT;
 
     static constexpr sqt::DataType DataType = sqt::DataType::Float;
     static constexpr bool IsNullable = false;
-    static constexpr std::size_t ParameterCount = 1;
 
-    static void BindValue(Statement& statement, int parameter_index, const T& value) {
+    /**
+    Binds a floating-point value to the statement at the specified parameter index.
+
+    @param statement
+        The statement to which the value is bound.
+
+    @param parameter_index
+        The index of the parameter to which the value is bound.
+
+    @param value
+        The value to bind.
+
+    @throw sqt::SQLError
+        Thrown if the binding fails.
+
+    @details
+        This method uses the `BindParameter(int, double)` method of the statement to bind the 
+        value.
+
+    @see sqt::Statement::BindParameter(int, double)
+    */
+    static void BindValue(Statement& statement, int parameter_index, FLOAT value) {
         statement.BindParameter(parameter_index, value);
     }
 
-    static T RetrieveValue(const Statement& statement, int column_index) {
-        return static_cast<T>(statement.GetColumnDouble(column_index));
+    /**
+    Retrieves a floating-point value from the statement at the specified column index.
+
+    @param statement
+        The statement from which the value is retrieved.
+
+    @param column_index
+        The index of the column from which to retrieve the value.
+
+    @return
+        The retrieved value.
+
+    @details
+        This method uses the `GetColumnDouble()` method to retrieve the value from the statement.
+
+        @warning
+        The retrieved value will be truncated if the original value stored in the database exceeds
+        the maximum representable value of the integral type.
+
+    @see sqt::Statement::GetColumnDouble()
+    */
+    static FLOAT RetrieveValue(const Statement& statement, int column_index) noexcept {
+        return static_cast<FLOAT>(statement.GetColumnDouble(column_index));
     }
 };
 
 
+/**
+The specialization for defining traits for `std::string`.
+
+@details
+    This specialization satisfies the `sqt::BasicValueTraitsType` concept.
+
+@see sqt::BasicValueTraitsType
+@see sqt::PrimitiveValueTraits<FLOAT>
+@see sqt::PrimitiveValueTraits<INTEGER>
+*/
 template<>
 class PrimitiveValueTraits<std::string> {
 public:
@@ -62,12 +214,52 @@ public:
 
     static constexpr sqt::DataType DataType = sqt::DataType::Text;
     static constexpr bool IsNullable = false;
-    static constexpr std::size_t ParameterCount = 1;
 
+    /**
+    Binds a string to the statement at the specified parameter index.
+
+    @param statement
+        The statement to which the string is bound.
+
+    @param parameter_index
+        The index of the parameter to which the string is bound.
+
+    @param value
+        The string to bind.
+
+    @throw sqt::SQLError
+        Thrown if the binding fails.
+
+    @details
+        This method uses the `BindParameter(int, std::string_view)` method of the statement to bind 
+        the string.
+
+    @see sqt::Statement::BindParameter(int, std::string_view)
+    */
     static void BindValue(Statement& statement, int parameter_index, const std::string& value) {
         statement.BindParameter(parameter_index, value);
     }
 
+    /**
+    Retrieves a string from the statement at the specified column index.
+
+    @param statement
+        The statement from which the string is retrieved.
+
+    @param column_index
+        The index of the column from which to retrieve the string.
+
+    @return
+        A copy of the string retrieved from the statement.
+
+    @throw std::bad_alloc
+        Thrown if the string cannot be allocated.
+
+    @details
+        This method uses the `GetColumnText()` method to retrieve the string from the statement.
+
+    @see sqt::Statement::GetColumnText()
+    */
     static std::string RetrieveValue(const Statement& statement, int column_index) {
         return std::string{ statement.GetColumnText(column_index) };
     }
