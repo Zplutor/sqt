30af05e4d119e1c508c0e776b5d19b9ee8f2bf64
zplutor <zplutor@qq.com>
2025-04-28 13:00:51 +0800

    Add documentation.

--- src/sqt/orm/table/column/column_type.h
+++ src/sqt/orm/table/column/column_type.h
@@ -1,11 +1,39 @@
 #pragma once
 
+/**
+@file
+    Defines the `sqt::ColumnType` concept.
+*/
+
 #include <concepts>
 #include <sqt/orm/table/column/abstract_column.h>
 #include <sqt/orm/table/column/value_source_type.h>
 
 namespace sqt {
 
+/**
+Constrains a type to be a column type.
+
+@details
+    @b Requirements
+    - The type must derive from `sqt::AbstractColumn`.
+    - The type must have a nested type `EntityType`, representing the entity type from which the 
+      column is mapped.
+    - The type must have a nested type `ValueType`, representing the value type of the column.
+    - The type must have a nested type `ValueTraits`, defining traits for the value type.
+    - The type must have a nested type `ValueSource`, which must satisfy the `sqt::ValueSourceType`
+      concept.
+    - The type must have a static constant `Name` of type `std::string_view`.
+
+    Columns defined by the `SQT_COLUMN_FIELD`, `SQT_COLUMN_ACCESSOR` and `SQT_COLUMN_CUSTOM` macros
+    satisfy this concept.
+
+@see sqt::AbstractColumn
+@see sqt::ValueSourceType
+@see SQT_COLUMN_ACCESSOR
+@see SQT_COLUMN_CUSTOM
+@see SQT_COLUMN_FIELD
+*/
 template<typename T>
 concept ColumnType = 
     std::is_base_of_v<AbstractColumn, T> && 
--- src/sqt/orm/table/identifier_type.h
+++ src/sqt/orm/table/identifier_type.h
@@ -1,10 +1,29 @@
 #pragma once
 
+/**
+@file
+    Defines the `sqt::IdentifierType` concept.
+*/
+
 #include <sqt/orm/table/column/column_type.h>
 #include <sqt/orm/table/column/composite_column_type.h>
 
 namespace sqt {
 
+/**
+Constrains a type to be an identifier type, which is a column type, primary key type or index type.
+
+@details
+    @b Requirements
+    - The type must satisfies the `sqt::ColumnType` or `sqt::CompositeColumnType` concept.
+
+    Identifiers can be used for expressions that require an identifier, such as assignment 
+    expressions.
+
+@see sqt::AssignmentType
+@see sqt::ColumnType
+@see sqt::CompositeColumnType
+*/
 template<typename T>
 concept IdentifierType = ColumnType<T> || CompositeColumnType<T>;
 
