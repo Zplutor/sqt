95a2c21bb2b1f170fd365abee8ebeb3524784c6d
zplutor <zplutor@qq.com>
2025-05-19 13:58:14 +0800

    Add documentation.

--- src/sqt/orm/expression/binder/binder.h
+++ src/sqt/orm/expression/binder/binder.h
@@ -1,9 +1,30 @@
 #pragma once
 
+/**
+@file
+    Defines the `sqt::Binder<>` class template.
+*/
+
 #include <sqt/orm/value/value_traits_type.h>
 
 namespace sqt {
 
+/**
+Provides information for binding values to a placeholder of an executor.
+
+@tparam TRAITS
+    The value traits type of the binder.
+
+@details
+    The `sqt::Binder<>` class template is used by the `sqt::BinderChain<>` class template to bind
+    values to placeholders in an executor. It provides the necessary binding information, such as 
+    the value traits type and the index of the placeholder.
+
+    This class template satisfies the `sqt::BinderType` concept.
+   
+@see
+    sqt::BinderType
+*/
 template<ValueTraitsType TRAITS>
 class Binder {
 public:
@@ -11,10 +32,22 @@ public:
     using ValueType = typename TRAITS::ValueType;
 
 public:
+    /**
+    Constructs a binder for the specified placeholder index.
+
+    @param index
+        The index of the placeholder to which the value will be bound.
+    */
     constexpr Binder(int index) noexcept : index_(index) {
 
     }
 
+    /**
+    Gets the index of the placeholder.
+
+    @return
+        The index of the placeholder.
+    */
     constexpr int Index() const noexcept {
         return index_;
     }
--- src/sqt/orm/expression/binder/binder_type.h
+++ src/sqt/orm/expression/binder/binder_type.h
@@ -1,16 +1,38 @@
 #pragma once
 
+/**
+@file
+    Defines the `sqt::BinderType` concept.
+*/
+
 #include <concepts>
 
 namespace sqt {
 
+/**
+Constrains a type to be a binder type, which provides information for binding values to a 
+placeholder of an executor.
+
+@details
+    @b Requirements
+    - The type must have a nested type `ValueType`.
+    - The type must have a nested type `ValueTraits`.
+    - The type must have a member method `Index()` with the following signature:
+      @code
+      int Index() const noexcept;
+      @endcode
+
+    The `sqt::Binder<>` class template satisfies this concept.
+
+@see sqt::Binder<>
+*/
 template<typename T>
 concept BinderType = requires {
     typename T::ValueType;
     typename T::ValueTraits;
 } &&
 requires(const T& t) {
-    { t.Index() } -> std::same_as<int>;
+    { t.Index() } noexcept -> std::same_as<int>;
 };
 
 }
